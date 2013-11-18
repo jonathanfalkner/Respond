2f2b1affb910364d17a908a345538f279a7e0d34
Jonathan Falkner <Jonathan.Falkner@Fynydd.com>
2013-11-18 09:42:23 -0500

    Updated to support pulling in stylesheets from the body of the HTML document (tested for IE 7 and 8).  This is achieved by first using IE conditional comments to change the mime type of the <style> tag to an invalid mime type (such as "text/x-OldIEIrritatesMe") so that IE 7 and 8 do not natively process the style tag.  With this in place within the document, Respond.js is able to find and parse style tags in the body of the document.  One future possible change would be to do the same for header stylesheets, as well as change the mime type (and set rel='alternate') for the link tags in the header, eliminating the double file pull for the stylesheets in IE8 and below, allowing Respond.js to pull the file instead.

--- respond.src.js
+++ respond.src.js
@@ -34,12 +34,116 @@ window.matchMedia = window.matchMedia || (function( doc, undefined ) {
 
 }( document ));
 
+/* if you already have jquery in use, or have a domready solution, this can be removed - just set var domReady = YourReadyFunction; to ensure interoperability */
+/*!
+ * Adaptation of the $(document).ready() function from jQuery
+ * library for use in simple JavaScript scenarios.
+ *
+ * --------------------------------------------------------------------- 
+ * jQuery JavaScript Library v1.4.3
+ * http://jquery.com/ 
+ *
+ * Copyright (c) 2010 John Resig, http://jquery.com/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * ----------------------------------------------------------------------
+ */
 
+var domReady = (function() {
 
+    var w3c = !!document.addEventListener,
+        loaded = false,
+        toplevel = false,
+        fns = [];
+    
+    if (w3c) {
+        document.addEventListener("DOMContentLoaded", contentLoaded, true);
+        window.addEventListener("load", ready, false);
+    }
+    else {
+        document.attachEvent("onreadystatechange", contentLoaded);
+        window.attachEvent("onload", ready);
+        
+        try {
+            toplevel = window.frameElement === null;
+        } catch(e) {}
+        if ( document.documentElement.doScroll && toplevel ) {
+            scrollCheck();
+        }
+    }
+
+    function contentLoaded() {
+        (w3c)?
+            document.removeEventListener("DOMContentLoaded", contentLoaded, true) :
+            document.readyState === "complete" && 
+            document.detachEvent("onreadystatechange", contentLoaded);
+        ready();
+    }
+    
+    // If IE is used, use the trick by Diego Perini
+    // http://javascript.nwbox.com/IEContentLoaded/
+    function scrollCheck() {
+        if (loaded) {
+            return;
+        }
+        
+        try {
+            document.documentElement.doScroll("left");
+        }
+        catch(e) {
+            window.setTimeout(arguments.callee, 15);
+            return;
+        }
+        ready();
+    }
+    
+    function ready() {
+        if (loaded) {
+            return;
+        }
+        loaded = true;
+        
+        var len = fns.length,
+            i = 0;
+            
+        for( ; i < len; i++) {
+            fns[i].call(document);
+        }
+    }
+    
+    return function(fn) {
+        // if the DOM is already ready,
+        // execute the function
+        return (loaded)? 
+            fn.call(document):      
+            fns.push(fn);
+    }
+})();
+/* END DOMREADY SCRIPT */
 
 
 /*! Respond.js v1.3.0: min/max-width media query polyfill. (c) Scott Jehl. MIT/GPLv2 Lic. j.mp/respondjs  */
-(function( win ){
+/*! Respond.js v1.3.0-f: forked to add support for style tags in the body.  Just use conditional IE statements to add a different style open tag (with a fake mimetype to break IE 7 and 8's auto-processing of stylesheets).
+NOTE: does not work with IE6, but gracefully fails with IE6.  Seriously though... IE6?
+ */
+(function( win,  ){
 
 	"use strict";
 
@@ -321,13 +425,32 @@ window.matchMedia = window.matchMedia || (function( doc, undefined ) {
 			return function(){
 				return xmlhttpmethod;
 			};
-		})();
+		})(),
+		addStyles = function () {
+        domReady(function () {
+            var styles = document.getElementsByTagName("body")[0].getElementsByTagName("style");
+            //below assumes that we've added an invalid mimetype to the style sheet for IE8 and older ("text/x-OldIEBrowsersIrritateMe" for example)
+            for (var i = 0; i < styles.length; i++) {
+                if (!!styles[i]&&!!styles[i].innerHTML&&!!styles[i].getAttribute("MQProcessed"){
+                    translate(
+                        styles[i].innerHTML,
+                        (window.location.href || window.location)
+                    );
+                    styles[i].setAttribute("MQProcessed","true");
+                }
+            }
+        });
+		};
 	
 	//translate CSS
 	ripCSS();
+	addStyles();
 	
 	//expose update for re-running respond later on
 	respond.update = ripCSS;
+	
+	//expose option for applying body styles (automatically waits for domready, gets style tags from the body, and processes them.
+	respond.addStyles = addStyles;
 	
 	//adjust on resize
 	function callMedia(){
